ef5cb3fcbf3950512e69986b1d49af48cb33292a
Chris Nain <chrisnain@hotmail.com>
2013-03-10 20:07:40 +0100

    cron schedule test

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Selectedrt::Application.initialize!
-
-config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
