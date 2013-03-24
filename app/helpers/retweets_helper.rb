ef47f7dd47993155f021e93044b2b98517a423b1
Chris Nain <chrisnain@hotmail.com>
2013-03-24 02:34:12 +0100

    exception handling for the 'end of file reached'-error, probably raised by twitter

--- app/helpers/retweets_helper.rb
+++ app/helpers/retweets_helper.rb
@@ -33,20 +33,28 @@ module RetweetsHelper
     tweetstatus = Array.new
     
     @accounts.each do |account|
-      # get tweets of definded accounts of the last day
-      result = Twitter.search("from:" + account.name + ", since:" + d, :result_type => "recent")
-      result.results.map do |status|
-        # tweet is no answer on anyone
-        if status.in_reply_to_tweet_id == nil
-          #tweet has correct url
-          status.urls.each do |url|
-            if url.expanded_url.include? account.url
-              tweetstatus << status
-            end
-          end  
+      begin
+        # get tweets of definded accounts of the last day
+        result = Twitter.search("from:" + account.name + ", since:" + d, :result_type => "recent")
+      rescue Twitter::Error::ClientError => e
+        flash.now.alert = e.message
+      end
+      
+      if result != nil
+        result.results.map do |status|
+          # tweet is no answer on anyone
+          if status.in_reply_to_tweet_id == nil
+            #tweet has correct url
+            status.urls.each do |url|
+              if url.expanded_url.include? account.url
+                tweetstatus << status
+              end
+            end  
+          end
         end
       end
     end
+
     return tweetstatus
   end
   
